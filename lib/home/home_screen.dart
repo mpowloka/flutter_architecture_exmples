ce309280676edb66ecd7f296ea4a4180df25bbb5
mpowloka <michalpowloka@interia.pl>
2019-03-02 15:49:24 +0100

    Create home screen

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_architecture_exmples/bloc/navigation/routes.dart';
+import 'package:flutter_architecture_exmples/mvp/navigation/routes.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
-      body: Center(
-        child: Row(
-          children: <Widget>[
-            RaisedButton(
-                child: Text("MVP Example"),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/mvpExample');
-                })
-          ],
+        appBar: AppBar(
+          title: Text("Home"),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                  child: Text("MVP Example"),
+                  onPressed: () {
+                    Navigator.push(context, MvpRoutes.getListOfItemsRoute());
+                  }),
+              RaisedButton(
+                  child: Text("BLoC Example"),
+                  onPressed: () {
+                    Navigator.push(context, BlocRoutes.getListOfItemsRoute());
+                  }),
+            ],
+          ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_architecture_exmples/bloc/item_list/bloc.dart';
+import 'package:flutter_architecture_exmples/bloc/providers/app_scope.dart';
+import 'package:flutter_architecture_exmples/datasource/items_repository.dart';
 import 'package:flutter_architecture_exmples/home/home_screen.dart';
-import 'package:flutter_architecture_exmples/mvp/item_list/item_list_view_impl.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Flutter Demo",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return AppScopeProvider(
+      itemListBloc: ItemListBloc(ItemsRepository()),
+      child: MaterialApp(
+        title: "Flutter Demo",
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomeScreen(),
       ),
-      routes: {
-        "/": (context) => HomeScreen(),
-        "/mvpExample": (context) => ItemListMvpScreen()
-      },
-      initialRoute: "/",
     );
   }
 }
